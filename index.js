77301f4e0b3d0f5f0f2461f3f66ecd106fd1a358
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2025-10-18 23:54:06 -0300

    up

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ function multiplicador() {
     const quantidadeDeSlot = 9;
     var imagens = [
         "./images/a001.jpg", "./images/a002.jpg", "./images/a003.jpg",
-        "./images/a004.jpg", "./images/a005.jpg", "./images/a006.jpg",
-        "./images/a007.jpg", "./images/a008.jpg", "./images/a009.jpg",
-        "./images/a010.jpg", "./images/a011.jpg",
+        "./images/a004.jpg", "./images/a005.png", "./images/a006.png",
+        "./images/a007.png", "./images/a008.png", "./images/a009.png",
+        "./images/a010.png", "./images/a011.png",
     ];
-    var pesos = [5, 1, 1, 0.5, 0.5, 0.5, 0.25, 0.25, 0.25, 0.10, 0.10, 0.10, 0.05, 0.05, 0.05];
-    var multiplicadores = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 8, 9];
+    var pesos = [0.8, 0.8, 0.8, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.10, 0.10];
+    var multiplicadores = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6];
     var resultados = [];
 
     var divImagens = document.querySelector(".images");
@@ -157,7 +157,7 @@ function multiplicador() {
             creditosValor += ganhoTotal;
             creditos.value = creditosValor;
             ganhos.value = ganhoTotal;
-            divResultado.innerHTML = "Não contava com minha astucia! Você ganhou " + ganhoTotal + " créditos!";
+            divResultado.innerHTML = "Você ganhou " + ganhoTotal + " créditos!";
             divResultado.classList = 'won';
 
             // Adiciona borda verde aos slots ganhadores
@@ -167,7 +167,7 @@ function multiplicador() {
             });
         } else {
             ganhos.value = 0;
-            divResultado.innerHTML = "(Qué que foi, Qué que foi, Qué que há!!! )";
+            divResultado.innerHTML = "Mais sorte na próxima vez!";
             divResultado.classList = 'lost';
         }
 
